29d8f4edd8d9adc489e866a6d58ee6e86d33f6d2
andy <andyphung64@gmail.com>
2025-02-20 19:06:42 -0500

    rahhhh

--- A_Joystick/controller_test.ino
+++ A_Joystick/controller_test.ino
@@ -1,9 +1,21 @@
-#include <RotaryEncoder.h>
 #include <Keyboard.h>
 
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
 
-// clk to a0, dt to a1
-RotaryEncoder encoder(A0, A1);
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 100;    // the debounce time; increase if the output flickers
+
+int buttonState2;            // the current reading from the input pin
+int lastButtonState2 = LOW;  // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime2 = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay2 = 100;    // the debounce time; increase if the output flickers
+
 
 void setup()
 {
@@ -11,6 +23,8 @@ void setup()
 
   // button pin
   pinMode(9, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
   
   Serial.begin(57600);
   
@@ -20,26 +34,107 @@ void setup()
 
 void loop()
 {
-  static int pos = 0;
-  encoder.tick();
-
-  int newPos = encoder.getPosition();
-  if (pos != newPos) {
-    Serial.print(newPos);
-    Serial.println();
-    if(newPos > pos) {
-      Keyboard.write(72);
-    }
-
-    if(newPos < pos) {
-      Keyboard.write(74);
-    }
-    pos = newPos;
-  }
 
   if(digitalRead(9) == LOW) { // spams paint/unpaint if you hold it ..
-    delay(250);
     Keyboard.write(83);
+    Serial.println("paint");
+    delay(300);
     
   }
+
+
+
+  
+
+  int reading2 = digitalRead(8);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading2 != lastButtonState2) {
+    // reset the debouncing timer
+    lastDebounceTime2 = millis();
+  }
+
+  if ((millis() - lastDebounceTime2) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    
+
+    // if the button state has changed:
+    if (reading2 != buttonState2) {
+      buttonState2 = reading2;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState2 == HIGH) {
+        Keyboard.write(65);
+        Serial.println("left 1");
+        //delay(500);
+      }
+    } else if(lastButtonState2 == HIGH && reading2 == HIGH) {
+      if((millis() - lastDebounceTime2) > debounceDelay * 5) {
+        delay(500);
+        Serial.println("left 2");
+        Keyboard.write(65);
+      }
+    }
+    
+  }
+
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState2 = reading2;
+
+
+
+
+  
+  
+
+  int reading = digitalRead(7);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        Keyboard.write(68);
+        Serial.println("right 1");
+        //delay(500);
+      }
+    } else if(lastButtonState == HIGH && reading == HIGH) {
+      if((millis() - lastDebounceTime) > debounceDelay * 5) {
+        delay(500);
+        Serial.println("right 2");
+        Keyboard.write(68);
+      }
+    }
+    
+  }
+
+  
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+  
 }
